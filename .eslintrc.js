7dfbd37b96ea3920f07e8e0c3b83f91a11c495e7
SeonghoJin <seongho4996@naver.com>
2021-06-29 22:52:11 +0900

    setting eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,17 @@
 module.exports = {
-    parser: "@typescript-eslint/parser",
-    plugins: ["@typescript-eslint"],
-    extends: [
-        // Airbnb style guide 적용
-        "airbnb-base",
-        // TypeScript ESLint recommanded style 적용
-        "plugin:@typescript-eslint/eslint-recommended"
-    ]
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    // Airbnb style guide 적용
+    'airbnb-base',
+    // TypeScript ESLint recommanded style 적용
+    'plugin:@typescript-eslint/eslint-recommended',
+  ],
+  rules: {
+    // note you must disable the base rule as it can report incorrect errors
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+  },
 };
