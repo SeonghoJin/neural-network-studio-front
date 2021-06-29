fe29c5cfcccf0ad2aa1e082d24225b6a2b7b0ab7
SeonghoJin <seongho4996@naver.com>
2021-06-29 23:00:09 +0900

    init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
+// eslint-disable-next-line no-use-before-define
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Main from "./Pages/main";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Main/>
     </div>
   );
 }
