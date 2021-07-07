d1e3c99cc244bf999227e01e5d75faa979dbf31c
SeonghoJin <seongho4996@naver.com>
2021-07-07 11:35:42 +0900

    chore : test5, test6 추가

--- src/Pages/test/react-flow/ReactFlowTest.tsx
+++ src/Pages/test/react-flow/ReactFlowTest.tsx
@@ -4,6 +4,8 @@ import test1 from './test1';
 import test2 from './test2';
 import test3 from './test3';
 import test4 from './test4';
+import test5 from './test5';
+import test6 from './test6';
 
 const ReactFlowTest = () => (
     <div>
@@ -11,6 +13,8 @@ const ReactFlowTest = () => (
         <Route path="/test/react-flow/2" exact={true} component={test2}></Route>
         <Route path="/test/react-flow/3" exact={true} component={test3}></Route>
         <Route path="/test/react-flow/4" exact={true} component={test4}></Route>
+        <Route path="/test/react-flow/5" exact={true} component={test5}></Route>
+        <Route path="/test/react-flow/6" exact={true} component={test6}></Route>
     </div>
 );
 
--- src/core/nodetypes/index.ts
+++ src/core/nodetypes/index.ts
@@ -1,10 +1,14 @@
 import TestNode1Component from './test/TestNode1Component';
+import TestNode2Component from './test/TestNode2Component';
 
 export enum CustomTypes {
     // eslint-disable-next-line no-unused-vars
-    testNode1 = 'testNode1'
+    testNode1 = 'testNode1',
+    // eslint-disable-next-line no-unused-vars
+    testNode2 = 'testNode2'
 }
 
 export const nodetypes = {
   [CustomTypes.testNode1]: TestNode1Component,
+  [CustomTypes.testNode2]: TestNode2Component,
 };
