fdfb2e911cc2a55cae831548a9ab958e693ff7d1
moretall <seongho4996@naver.com>
2021-07-27 00:50:14 +0900

    feat : SliderInput 추가

---
+++ src/Components/Input/SliderInput.tsx
@@ -0,0 +1,51 @@
+import { makeStyles, Slider, TextField, Typography } from '@material-ui/core';
+import Input from './Input';
+import { ChangeEvent } from 'react';
+
+const useStyle = makeStyles({
+  propertyContentContainer: {
+    width: '100%',
+  },
+});
+
+type Props = {
+  propertyName: string,
+  propertyContent: number,
+  onChange: (e: ChangeEvent<any>) => void,
+  min: number,
+  max: number,
+  step: number,
+};
+
+const SliderInput = (props : Props) => {
+  const classes = useStyle();
+  const {propertyContent, propertyName, max, min, step, onChange} = props
+  const body = (
+    <Slider
+      className={classes.propertyContentContainer}
+      value={propertyContent}
+      name={propertyName}
+      step={step}
+      min={min}
+      max={max}
+      marks={true}
+      onChange={onChange}
+      valueLabelDisplay={'auto'}
+    >
+    </Slider>
+  )
+
+  const head = (
+    <Typography gutterBottom>
+      {propertyName}
+    </Typography>
+  );
+
+  return (
+    <Input
+      body={body}
+      head={head}
+    />)
+};
+
+export default SliderInput;
