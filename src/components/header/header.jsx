6fa18b5036e67d8da02c2408e18f43c1fb71eb81
moretall <seongho4996@naver.com>
2021-07-27 21:53:34 +0900

    fix : 로그인후 새로고침시 로그인, 로그아웃 컴포넌트가 깜박거리는 것 수정

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,58 +1,27 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Profile from './profile/profile';
 import Logo from './logo';
 import Auth from './auth/auth';
 import style from './index.module.css';
 
+const Header = (props) => {
 
-class Header extends React.Component {
-    state = {
-        auth: null,
-        loading: true,
-    }
+    const {auth, user, children, loading} = props;
 
-    componentDidMount() {
-        const {auth, user} = this.props;
-        console.log(auth);
-        this.setState({
-            auth: auth,
-        })
-    }
-
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        console.log(this.state.auth);
-        if (prevState.auth !== this.state.auth) {
-            this.setState({
-                loading: false,
-            })
-        }
-    }
-
-    render() {
-        const {auth, user} = this.props;
-        console.log(this.state.loading);
-        return(
-            <header className={`${style.topHeader}`}>
-                <div className={`${style.headerWrapper}`}>
-                    <Logo />
-                    <div className="top-center">
-                        {this.props.children}
-                    </div>
-                    <div className="top-right">
-                        {
-                            this.state.loading ?
-                                null:
-                                (
-                                    auth ?
-                                    <Profile user={user}/> :
-                                    <Auth />
-                                )
-                        }
-                    </div>
+    return (
+        <header className={`${style.topHeader}`}>
+            <div className={`${style.headerWrapper}`}>
+                <Logo />
+                <div className="top-center">
+                    {children}
                 </div>
-            </header>
+                <div className="top-right">
+                    {loading && "loading..."}
+                    {!loading && (auth ? <Profile user={user}/> : <Auth/>)}
+                </div>
+            </div>
+        </header>
         );
-    }
 }
 
 export default Header;
