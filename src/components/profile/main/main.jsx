fa039c206ebc872ee0f36dcd211e434001992670
elixter <ltw971@naver.com>
2021-07-29 17:41:30 +0900

    feat: user info change

--- src/components/profile/main/main.jsx
+++ src/components/profile/main/main.jsx
@@ -1,44 +1,128 @@
 import React from 'react';
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import { faGlobe, faLink } from '@fortawesome/free-solid-svg-icons'
-import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
-import { Link } from 'react-router-dom';
-import profile from '../../header/profile/default-profile.png';
+import Header from './header';
+import axios from "axios";
 
 import style from './main.module.css';
-
+import UserInfo from "./userInfo";
 
 class Main extends React.PureComponent {
-    render() {
-        const { user,  } = this.props;
+    state = {
+        modify: false,
+        email: this.props.user.email,
+        webSite: this.props.user.webSite,
+        description: this.props.user.description,
+        profileImage: this.props.user.profileImage,
+        name: this.props.user.name,
+        user: this.props.user,
+    }
 
+    getUser = async () => {
+        await axios({
+            method:"GET",
+            url: "/api/user",
+            withCredentials: true,
+        }).then((res) => {
+            this.setState({
+                auth: true,
+                user: res.data,
+                loading: false,
+            })
+        }).catch(err => {
+            this.setState({
+                auth: false,
+                loading: false,
+            })
+            alert('로그인이 필요합니다.');
+            this.props.history.push('/login');
+        })
+    }
+
+    onPressButton = async () => {
+        if (!this.state.modify) {
+            this.setState({
+                modify: true,
+            })
+        }
+        else {
+            if (window.confirm('저장하시겠습니까?')) {
+                await axios({
+                    method:"PUT",
+                    url: "/api/user",
+                    data: {
+                        "profileImage": this.state.profileImage,
+                        "description": this.state.description,
+                        "name": this.state.name,
+                        "email": this.state.email,
+                        "webSite": this.state.webSite,
+                    },
+                }).then((res) => {
+                    this.getUser();
+                }).catch(err => {
+                    alert('something wrong!');
+                });
+                this.setState({
+                    modify: false,
+                })
+            }
+        }
+    }
+
+    onPressCancel = () => {
+        this.setState({
+            modify: false,
+        })
+    }
+
+    setEmail = (email) => {
+        this.setState({
+            email: email,
+        })
+        console.log(this.state.email);
+    }
+
+    setWebSite = (webSite) => {
+        this.setState({
+            webSite: webSite,
+        })
+    }
+
+    setDescription = (description) => {
+        this.setState({
+            description: description,
+        })
+    }
+
+    setName = (name) => {
+        this.setState({
+            name: name,
+        })
+    }
+
+    render() {
         return (
             <div className={`${style.mainWrapper}`}>
-                <header>
-                    <div className={`${style.profile}`}>
-                        <div className={`${style.profileImage}`}>
-                            <img src={user.profileImage === "" ? profile : user.profileImage} />
-                        </div>
-                        <h2>{user.name}</h2>
+                <Header user={this.state.user} modify={this.state.modify} onChangeName={this.setName}>
+                    <div className={`${style.buttons}`}>
+                        {
+                            this.state.modify ?
+                                <button className={`${style.modifyButton}`} onClick={this.onPressCancel}>취소</button>:
+                                null
+                        }
+                        <button className={`${style.modifyButton}`} onClick={this.onPressButton}>
+                            {
+                                this.state.modify ?
+                                    '저장' :
+                                    '수정'
+                            }
+                        </button>
                     </div>
-                </header>
-                <section className={`${style.userInfo}`}>
-                    <div className={`${style.userMail}`}>
-                        <FontAwesomeIcon icon={faEnvelope} color={'gray'} />
-                        <a href={`mailto:ltw971@naver.com`}>
-                            ltw971@naver.com
-                        </a>
-                    </div>
-                    <div className={`${style.userWebsite}`}>
-                        <FontAwesomeIcon icon={faGlobe} color={'gray'} />
-                        <a href={'http://github.com/elixter'} target={'_blank'}>
-                            http://github.com/elixter
-                        </a>
-                    </div>
-                    <div className={`${style.userDescription}`}>
-                        <p>{user.description}</p>
-                    </div>
-                </section>
+                </Header>
+                <UserInfo user={this.state.user}
+                          modify={this.state.modify}
+                          onChangeEmail={this.setEmail}
+                          onChangeWebSite={this.setWebSite}
+                          onChangeDescription={this.setDescription}
+                />
                 <footer>
                     프리셋 목록 들어감
                 </footer>
