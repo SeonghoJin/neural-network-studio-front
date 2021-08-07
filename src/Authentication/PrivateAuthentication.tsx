69d4d950b4f5aa46138fc3865ea4b1467ceb3b41
moretall <seongho4996@naver.com>
2021-08-07 23:16:11 +0900

    feat : 사용자가 로그아웃이 main 페이지로 리다이렉션

--- src/Authentication/PrivateAuthentication.tsx
+++ src/Authentication/PrivateAuthentication.tsx
@@ -1,6 +1,8 @@
 import { Redirect } from 'react-router-dom';
 import { useCallback } from 'react';
 import useGetUserProfileResult from '../hooks/APIResult/user/useGetUserProfileResult';
+import useAuthentication from '../hooks/useAuthentication';
+import { UserType } from '../module/Auth';
 
 type Props = {
 	children: any;
@@ -8,11 +10,14 @@ type Props = {
 
 const PrivateAuthentication = ({ children }: Props) => {
 	const { data, error } = useGetUserProfileResult();
-
+	const { user } = useAuthentication();
 	const errorComponent = useCallback(() => {
-		alert('로그인이 필요합니다.');
-		return <Redirect to="/login" />;
-	}, []);
+		if (user.type === UserType.Visitor) {
+			alert('로그인이 필요합니다.');
+			return <Redirect to="/login" />;
+		}
+		return <Redirect to="/" />;
+	}, [user]);
 
 	return (
 		<>
