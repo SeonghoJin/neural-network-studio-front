d4b3445961e8c3f68c5521223d50068592a1b750
moretall <seongho4996@naver.com>
2021-09-08 18:14:35 +0900

    chore : folder 구조 변경 및 필요없는 코드 삭제

--- src/core/Socket/Context/SocketContext.tsx
+++ src/core/Socket/Context/SocketContext.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { SocketService } from '../SocketService/SocketService';
 import { SocketRepository } from '../SocketRepository/SocketRespository';
 
