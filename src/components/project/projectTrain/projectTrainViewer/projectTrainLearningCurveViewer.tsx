41279a7055502f1b6cc0f8da01c790b766cc090e
elixter <ltw971@naver.com>
2021-10-13 23:01:56 +0900

    feat: train page prototype

--- src/components/project/projectTrain/projectTrainViewer/projectTrainLearningCurveViewer.tsx
+++ src/components/project/projectTrain/projectTrainViewer/projectTrainLearningCurveViewer.tsx
@@ -2,7 +2,7 @@ import { Line } from 'react-chartjs-2';
 
 const ProjectTrainLearningCurveViewer = (epochs: any) => {
 	const data = {
-		labels: ['1', '2', '3', '4', '5', '6'],
+		labels: [1, 2, 3, 4, 5, 6],
 		datasets: [
 			{
 				label: '# of Votes',
